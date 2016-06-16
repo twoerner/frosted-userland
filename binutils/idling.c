047156aa153fcc9db98f885e8d039cba5e47f8a3
Trevor Woerner <twoerner@gmail.com>
2016-06-16 09:09:30 -0400

    idling.c: just flash 1 LED
    Signed-off-by: Trevor Woerner <twoerner@gmail.com>

--- binutils/idling.c
+++ binutils/idling.c
@@ -26,32 +26,16 @@
 
 int main(int argc, char *argv[])
 {
-    int pid;
-    int led[4];
-    int i, j;
+    int ledFd;
+    uint8_t val = '0';
 
-# define LED0 "/dev/led0"
-# define LED1 "/dev/led1"
-# define LED2 "/dev/led2"
-# define LED3 "/dev/led3"
+    ledFd = open("/dev/led0", O_RDWR, 0);
 
-    led[0] = open(LED0, O_RDWR, 0);
-    led[1] = open(LED1, O_RDWR, 0);
-    led[2] = open(LED2, O_RDWR, 0);
-    led[3] = open(LED3, O_RDWR, 0);
-
-    if (led[0] >= 0) {
-        while(1) {
-            for (i = 0; i < 9; i++) {
-                if (i < 4) {
-                    write(led[i], "0", 1);
-                } else {
-                    char val = (1 - (i % 2)) + '0';
-                    for(j = 0; j < 4; j++)
-                        write(led[j], &val, 1);
-                }
+    if (ledFd >= 0) {
+        while (1) {
+        	write(ledFd, &val, 1);
+        	val ^= 1;
                 usleep(200000);
-            }
         }
     } else {
         while(1) { sleep(1); } /* GPIO unavailable, just sleep. */
